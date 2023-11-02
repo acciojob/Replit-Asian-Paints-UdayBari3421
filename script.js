43185b2c46c7438883953fae85a758be943b34a6
UdayBari3421 <115573575+UdayBari3421@users.noreply.github.com>
2023-11-02 19:24:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const div = document.getElementById("3");
+const button = document.getElementById("change");
+div.style.backgroundColor = "Red";
+
+function func(params) {
+	div.style.backgroundColor = "transparent";
+};
